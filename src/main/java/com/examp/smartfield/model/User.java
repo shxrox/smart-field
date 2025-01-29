88d847e3fe9a1263cb3bed597c88dc485dd8a5d5
shxrox <sharondeva8@gmail.com>
2025-01-29 11:17:31 +0530

    Validation

--- src/main/java/com/examp/smartfield/model/User.java
+++ src/main/java/com/examp/smartfield/model/User.java
@@ -1,3 +1,104 @@
+//package com.examp.smartfield.model;
+//
+//import jakarta.persistence.*;
+//import jakarta.validation.constraints.Email;
+//import jakarta.validation.constraints.NotBlank;
+//import jakarta.validation.constraints.Size;
+//
+//@Entity
+//@Table(name = "users")
+//public class User {
+//    public Long getId() {
+//        return id;
+//    }
+//
+//    public void setId(Long id) {
+//        this.id = id;
+//    }
+//
+//    public @NotBlank(message = "Name is required") String getName() {
+//        return name;
+//    }
+//
+//    public void setName(@NotBlank(message = "Name is required") String name) {
+//        this.name = name;
+//    }
+//
+//    public @Email(message = "Email should be valid") @NotBlank(message = "Email is required") String getEmail() {
+//        return email;
+//    }
+//
+//    public void setEmail(@Email(message = "Email should be valid") @NotBlank(message = "Email is required") String email) {
+//        this.email = email;
+//    }
+//
+//    public @NotBlank(message = "Password is required") @Size(min = 6, message = "Password must be at least 6 characters") String getPassword() {
+//        return password;
+//    }
+//
+//    public void setPassword(@NotBlank(message = "Password is required") @Size(min = 6, message = "Password must be at least 6 characters") String password) {
+//        this.password = password;
+//    }
+//
+//    public @NotBlank(message = "Password confirmation is required") String getConfirmPassword() {
+//        return confirmPassword;
+//    }
+//
+//    public void setConfirmPassword(@NotBlank(message = "Password confirmation is required") String confirmPassword) {
+//        this.confirmPassword = confirmPassword;
+//    }
+//
+//    public Role getRole() {
+//        return role;
+//    }
+//
+//    public void setRole(Role role) {
+//        this.role = role;
+//    }
+//
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Long id;
+//
+//    @NotBlank(message = "Name is required")
+//    private String name;
+//
+//    @Email(message = "Email should be valid")
+//    @NotBlank(message = "Email is required")
+//    @Column(unique = true)
+//    private String email;
+//
+//    @NotBlank(message = "Password is required")
+//    @Size(min = 6, message = "Password must be at least 6 characters")
+//    private String password;
+//
+//    @Transient // Not persisted in the database
+//    @NotBlank(message = "Password confirmation is required")
+//    private String confirmPassword;
+//
+//    @Enumerated(EnumType.STRING)
+//    private Role role;
+//
+//    public enum Role {
+//        FARMER,
+//        CUSTOMER,
+//        ADMIN
+//    }
+//
+//    public User() {
+//    }
+//
+//    public User(Long id, String name, String password, String confirmPassword, Role role, String email) {
+//        this.id = id;
+//        this.name = name;
+//        this.password = password;
+//        this.confirmPassword = confirmPassword;
+//        this.role = role;
+//        this.email = email;
+//    }
+//
+//
+//}
 package com.examp.smartfield.model;
 
 import jakarta.persistence.*;
